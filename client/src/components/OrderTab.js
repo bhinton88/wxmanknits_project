bc5ab67123d740d778157470fec7e71d102e32a4
Bobby Hinton <bob.g.hint@gmail.com>
2023-06-30 12:29:10 -0500

    fixed order tab with new order info for payment status

--- client/src/components/OrderTab.js
+++ client/src/components/OrderTab.js
@@ -10,12 +10,14 @@ function OrderTab() {
     .then(response => response.json())
     .then(data => setOrders(data))
   }, [])
+
+  console.log(orders)
   
   return (
     <div className='d-flex flex-column align-items-center'>
       <h1>Completed Orders:</h1>
       {
-        orders.map(order => order.payment_status === "paid" ? <OrderCard key={order.id} order={order} /> : null)
+        orders.map(order => order.payment.status === "paid" ? <OrderCard key={order.id} order={order} /> : null)
       }
     </div>
   )
